46de8e6f42c25d494d7d2537dd1094fd45f0f0b8
copa777 <copadalessandro@gmail.com>
2025-10-26 18:47:01 -0400

    fetch:  agregacion de clientes, bitacora y permisos

--- back_2doParcial/Api_2doParcial/urls.py
+++ back_2doParcial/Api_2doParcial/urls.py
@@ -20,4 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/usuarios/', include('Usuarios.urls')),
+    path('api/permisos/', include('Permisos.urls')),
+    path('api/clientes/', include('Clientes.urls')),
 ]
