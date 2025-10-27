fd6771a27e73ce928868463e146f03d8628ff23d
copa777 <copadalessandro@gmail.com>
2025-10-27 12:47:09 -0400

    fix: arreglo de los empleados, sacandolo de los usuarios y agregandolo en un app propia

--- back_2doParcial/Api_2doParcial/urls.py
+++ back_2doParcial/Api_2doParcial/urls.py
@@ -21,5 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/usuarios/', include('Usuarios.urls')),
     path('api/permisos/', include('Permisos.urls')),
-    path('api/clientes/', include('Clientes.urls')),
+    path('api/clientes/', include('Cliente.urls')),
+    path('api/empleados/', include('Empleados.urls')),
+    path('api/bitacora/', include('Bitacora.urls')),
 ]
